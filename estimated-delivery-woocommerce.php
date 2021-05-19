d8c3c598c7cdda4d583aca1e0c24607f0ef0a9c8
Daniel Riera <danielriera.p@gmail.com>
2021-05-19 09:54:11 +0200

    Update estimated-delivery-woocommerce.php

--- estimated-delivery-woocommerce.php
+++ estimated-delivery-woocommerce.php
@@ -4,7 +4,7 @@
  * Description: Show estimated / guaranteed delivery, simple and easy
  * Author: TaxarPro
  * Author URI: https://taxarpro.com
- * Version: 1.1.2
+ * Version: 1.1.3
  * Text Domain: estimated-delivery-for-woocommerce
  * Domain Path: /languages
  * WC requires at least: 3.0
@@ -17,7 +17,7 @@ if(!defined('ABSPATH')) { exit; }
 define('EDW_PATH', dirname(__FILE__).'/');
 define('EDW_POSITION_SHOW', get_option('_edw_position', 'woocommerce_after_add_to_cart_button'));
 define('EDW_USE_JS', get_option('_edw_cache', '0'));
-define('EDW_Version', '1.1.2');
+define('EDW_Version', '1.1.3');
 
 require_once EDW_PATH . 'class.api.php';
 
